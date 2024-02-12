25e1882cc0bdfa3453e2f4cf8cd9d42a2c09fced
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-12 18:57:05 +0100

    add test plot

--- src/plot.R
+++ src/plot.R
@@ -1,1 +1,24 @@
 # script for plotting results of the simulations
+pacman::p_load(tidyverse)
+
+# load data
+datafilepath = file.path("out", "data.csv")
+df = read_csv(datafilepath)
+
+# sort to only look at WSLS_RL for now (combination col)
+RL_WSLS_df = df %>% filter(combination == "RL_WSLS")
+
+# compute average performance for each trial and agent
+average = RL_WSLS_df %>%
+    group_by(trial, agent_self_type) %>%
+    summarise(average_performance = mean(feedback))
+
+# create plot
+plot <- ggplot(average, aes(x = trial, y = average_performance, color = agent_self_type, group = agent_self_type)) +
+    geom_line() +
+    geom_point() +
+    labs(x = "Trial", y = "Average Performance", color = "Agent") +
+    theme_bw()
+
+# save the plot
+ggsave(file.path("out", "plot_test.jpeg"), plot, width = 8, height = 6)
