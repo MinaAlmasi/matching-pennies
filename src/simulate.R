6f37bdd4a34ad2f6f82c850eac746f6d495c1287
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-12 14:12:38 +0100

    add RL play game fn

--- src/simulate.R
+++ src/simulate.R
@@ -35,12 +35,46 @@ play_game_WSLS <- function(n_trials){
 
 }
 
-# play 
-dfs <- play_game_WSLS(10)
+play_game_RL <- function(n_trials) {
+    # init arrays
+    value_hider <- array(NA, n_trials)
+    choices_hider <- array(NA, n_trials)
+    feedback_hider <- array(NA, n_trials)
 
-hider_df <- dfs[[1]]
-picker_df <- dfs[[2]]
+    value_picker <- array(NA, n_trials)
+    choices_picker <- array(NA, n_trials)
+    feedback_picker <- array(NA, n_trials)
 
-print(hider_df)
-print(picker_df)
+    # init first trial vals for both agents
+    values_hider[1] <- 0
+    values_picker[1] <- 0
+    
+    choices_hider[1] <- rbinom(1,1,0.5) # 0.5 = sigmoid(0)
+    choices_picker[1] <- rbinom(1,1,0.5)
+
+    feedback_hider[1] <- ifelse(choices_hider[1] != choices_picker[1], 1, 0)
+    feedback_picker[1] <- ifelse(choices_hider[1] == choices_picker[1], 1, 0)
+
+    for (i in 2:n_trials){
+        # make agents
+        hider <- REINFORCEMENT_Agent(previous_choice = choices_hider[i-1], previous_value = values_hider[i-1], feedback = feedback_hider[i-1], alpha = 0.1)
+        picker <- REINFORCEMENT_Agent(previous_choice = choices_picker[i-1], previous_value = values_picker[i-1], feedback = feedback_picker[i-1], alpha = 0.1)
+
+        # get choices for hider and picker
+        choices_hider[i] <- hider[[1]]
+        choices_picker[i] <- picker[[1]]
+
+        # get value 
+        value_hider[i] <- hider[[2]]
+        value_picker[i] <- picker[[2]]
+
+        feedback_hider[i] <- ifelse(choices_hider[i] != choices_picker[i], 1, 0)
+        feedback_picker[i] <- ifelse(choices_hider[i] == choices_picker[i], 1, 0)
+    }
+    # bind the informaion to dataframes
+    hider_df <- data.frame(choices_hider, feedback_hider, values_hider)
+    picker_df <- data.frame(choices_picker, feedback_picker, values_picker)
+
+    return(list(hider_df, picker_df))
+}
 
