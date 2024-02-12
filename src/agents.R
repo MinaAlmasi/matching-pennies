9ae644f63863dc482e6f340cd28a4a2579cf3427
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-12 13:59:11 +0100

    rm is null statements

--- src/agents.R
+++ src/agents.R
@@ -3,10 +3,7 @@ pacman::p_load(tidyverse)
 
 # WIN SHIFT LOSE STAY AGENT
 WSHIFTLSTAY_Agent <- function(previous_choice, feedback){
-    # if we are in the first round (no previous choice and feedback), make a random selection
-    if (is.null(feedback) & is.null(previous_choice)){
-        choice = rbinom(1, 1, 0.5)
-    }
+
     # if we had a win last round, we do the opposite this round!
     if (feedback == 1){
         choice = 1 - previous_choice
@@ -22,34 +19,22 @@ WSHIFTLSTAY_Agent <- function(previous_choice, feedback){
 
 # REINFORCEMENT LEARNING AGENT
 REINFORCEMENT_Agent <- function(previous_choice, previous_value, feedback, alpha){
-    # if we are in the first round, define expected value 
-    if (is.null(previous_value)){
-        # set the zero as that is a 50/50 when put through sigmoid
-        new_value = 0.5
-    }
+    # compute previous correct choice from feeddback and previous choice (if feedback is 1, previous correct choice is previous choice, otherwise it is the opposite)
+    previous_correct_choice = ifelse(feedback==1, previous_choice, 1-previous_choice)
 
-    else {
-        # compute previous correct choice from feeddback and previous choice (if feedback is 1, previous correct choice is previous choice, otherwise it is the opposite)
-        previous_correct_choice = ifelse(feedback==1, previous_choice, 1-previous_choice)
+    # calculate prediction error
+    prediction_error = previous_correct_choice - previous_value
 
-        # calculate prediction error
-        prediction_error = previous_correct_choice - previous_value
-
-        # update value
-        new_value = previous_value + alpha * prediction_error
-    }
-
+    # update value
+    new_value = previous_value + alpha * prediction_error
+    
     # put value through a sigmoid using the formula
     choice_rate = 1/(1+exp(-new_value))
 
     # make choice based on value
-    choice = rbinom(1, 1, choice_rate)
-    #choice = ifelse(choice_rate > 0.5, 1, 0)
+    #choice = rbinom(1, 1, choice_rate)
+    choice = ifelse(choice_rate > 0.5, 1, 0)
 
     # return choice and value
     return(list(choice, new_value, choice_rate))
 }
-
-# testing the reinforcement agent
-agent = REINFORCEMENT_Agent(previous_choice = 1, previous_value = 0.9, feedback = 1, alpha = 0.5)
-print(agent[2])
