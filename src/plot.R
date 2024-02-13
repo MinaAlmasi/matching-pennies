538e2705ea1d957998210bbb59502c7109c18a3f
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-13 12:33:50 +0100

    combine all games (with diff learning rates)

--- src/plot.R
+++ src/plot.R
@@ -10,19 +10,21 @@ RL_WSLS_df = df %>% filter(combination == "RL_WSLS")
 
 # compute average performance for each trial and agent
 average = RL_WSLS_df %>%
-    group_by(trial, agent_self_type) %>%
-    summarise(average_performance = mean(feedback))
+    group_by(trial, agent_self_type, learning_rate) %>%
+    summarise(average_performance = mean(feedback)) # mean corresponds to calculating proportion
 
 # convert trial to integer (for adjusting x-axis)
 average$trial <- as.integer(average$trial)
+average$learning_rate <- as.factor(average$learning_rate)
 
 # create plot
 plot <- ggplot(average, aes(x = trial, y = average_performance, color = agent_self_type, group = agent_self_type)) +
     geom_line() +
     geom_point() +
-    labs(x = "Trial", y = "Average Performance", color = "Agent") +
+    labs(x = "Trial", y = "Proportion of wins at trial", color = "Agent") +
     scale_x_continuous(breaks = seq(0, 120, 10)) +
+    facet_grid(learning_rate~.) +
     theme_bw()
 
 # save the plot
-ggsave(file.path("out", "plot_RL_WSLS.jpeg"), plot, width = 8, height = 6)
+ggsave(file.path("out", "plot_RL_WSLS.jpeg"), plot, width = 12, height = 6)
--- src/simulate.R
+++ src/simulate.R
@@ -12,9 +12,6 @@ set.seed(2502)
 
 # trial number, choice, feedback, value, agent_self_type, agent_other_type, role, combination, game_id
 simulate_games <- function(n_trials, n_games){
-    # define LR for RL agents
-    learning_rate = 0.2
-
     # init games df
     games_df <- data.frame()
 
@@ -41,7 +38,7 @@ simulate_games <- function(n_trials, n_games){
 
     print("Playing RL-RL")
     for (i in 1:n_games){
-        dfs <- play_game_RL(n_trials, learning_rate=learning_rate)
+        dfs <- play_game_RL(n_trials, learning_rate=0.2)
 
         hider_df <- dfs[[1]]
         picker_df <- dfs[[2]]
@@ -58,26 +55,32 @@ simulate_games <- function(n_trials, n_games){
         # add to main games df
         games_df <- bind_rows(games_df, combined_df)
     }
-    print("Playing RL-WSLS")
-    for (i in 1:n_games){
-        dfs <- play_game_RL_WSLS(n_trials, learning_rate=learning_rate)
-
-        hider_df <- dfs[[1]]
-        picker_df <- dfs[[2]]
-
-        # combine hider_df and picker_df
-        combined_df <- bind_rows(hider_df, picker_df)
-
-        # add game_id
-        combined_df["game_id"] <- i
-        
-        # add combination
-        combined_df["combination"] <- "RL_WSLS"
-        
-        # add to main games df
-        games_df <- bind_rows(games_df, combined_df)
-    }
     
+    print("Playing RL-WSLS")
+    # define the learning rates we want to test
+    learning_rates <- c(0.05, 0.2, 0.5)
+
+    for (learning_rate in learning_rates){
+        print(paste("Playing RL-WSLS with learning rate", learning_rate))
+        for (i in 1:n_games){
+            dfs <- play_game_RL_WSLS(n_trials, learning_rate=learning_rate)
+            
+            hider_df <- dfs[[1]]
+            picker_df <- dfs[[2]]
+
+            # combine hider_df and picker_df
+            combined_df <- bind_rows(hider_df, picker_df)
+
+            # add game_id
+            combined_df["game_id"] <- i
+            
+            # add combination
+            combined_df["combination"] <- "RL_WSLS"
+            
+            # add to main games df
+            games_df <- bind_rows(games_df, combined_df)
+        }
+    }
     return(games_df)
 
 }
@@ -88,5 +91,5 @@ games_df <- simulate_games(120, 100)
 #print(games_df %>% filter(combination == "RL_WSLS") %>% head())
 
 # save to csv
-savefile <- file.path("out", "data_lr_02.csv")
+savefile <- file.path("out", "data.csv")
 write_csv(games_df, savefile)
